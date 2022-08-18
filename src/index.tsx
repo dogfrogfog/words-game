0f5c92b0e1c4523d25669c7cc5d7bc4521564d35
NikitaKakurin <neyasut87@rambler.ru>
2022-08-18 22:10:27 +0300

    feat: add tailwind an fix package.json -format

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'index.css';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <h1>Hello world!</h1>
-    </div>
-  );
-};
+const App = () => (
+  <div className='App'>
+    <h1 className='text-5xl font-bold text-green-400'>Hello world!</h1>
+  </div>
+);
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(

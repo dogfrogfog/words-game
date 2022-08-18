af2efa7b2b63f0a4a63de6b0f98e57569ec741e9
Andrew Rafalsky <andraffal@gmail.com>
2022-08-18 20:09:20 +0300

    fix: fix eslint and prettier configure

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello world!</h1>
     </div>
   );
-};
+}
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );

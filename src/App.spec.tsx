8b3b021fc046ff05ae2084e71efbe05c67fc499f
thelastandrew <thelastandrew@gmail.com>
2022-08-19 23:28:58 +0300

    fix: after review

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,17 +1,14 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 describe('Component: App', () => {
-  it('renders without crashing', () => {
-    const div = createRoot(document.createElement('div'));
-    div.render(<App />);
+  it('should successfully render component', () => {
+    render(<App />);
   });
   it('contains "Hello world!"', () => {
-    const { getByText } = render(<App />);
-    const textElement = getByText(/hello world/i);
-    expect(textElement).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByText('Hello world!')).toBeInTheDocument();
   });
 });

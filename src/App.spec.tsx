3974eb13e3a023aa26e2abff3185872757d0d99e
Andrey Fedotov <thelastandrew@gmail.com>
2022-08-19 09:06:48 +0300

    feat: add test test for App

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,3 +1,5 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -6,5 +8,10 @@ describe('Component: App', () => {
   it('renders without crashing', () => {
     const div = createRoot(document.createElement('div'));
     div.render(<App />);
+  });
+  it('contains "Hello world!"', () => {
+    const { getByText } = render(<App />);
+    const textElement = getByText(/hello world/i);
+    expect(textElement).toBeInTheDocument();
   });
 });

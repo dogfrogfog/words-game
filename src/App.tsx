12de235752dd87f9e18868785e89eabc8e75124e
NikitaKakurin <neyasut87@rambler.ru>
2022-08-20 13:47:03 +0300

    feat: create page home.tsx and move APP content there

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import Footer from 'components/footer/Footer';
-import Header from 'components/header/Header';
-import Main from 'components/main/Main';
+import Home from 'pages/home';
 import React from 'react';
 
-const App = () => (
-  <div className='flex flex-col justify-between h-screen pt-[70px] relative'>
-    <Header />
-    <Main />
-    <Footer />
-  </div>
-);
+const App = () => <Home />;
 
 export default App;

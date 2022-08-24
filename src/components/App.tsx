e8ca35cd2737b5c208b973c11b42cd6fa7d2499a
thelastandrew <thelastandrew@gmail.com>
2022-08-24 05:40:20 +0300

    feat: add modal window template

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,17 @@
 import Home from 'pages/home';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
+import Modal from './Modal';
 
 const App = () => (
+  // const [modalActive, setModalActive] = useState(true);
   <div className='flex flex-col justify-between h-screen pt-[70px] relative'>
     <Header />
     <main className='flex justify-between px-2 h-full'>
       <Home />
     </main>
     <Footer />
+    <Modal />
   </div>
 );
 
---
+++ src/components/Modal.tsx
@@ -0,0 +1,7 @@
+const Modal = () => (
+  <div className='w-screen h-screen fixed inset-x-0 inset-y-0 bg-black bg-opacity-50 flex justify-center items-center'>
+    <div className='p-5 bg-white rounded-xl'>Modal content</div>
+  </div>
+);
+
+export default Modal;

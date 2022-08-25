8b75c5095bf98a2da47a9496c7d46e74fc767f39
thelastandrew <thelastandrew@gmail.com>
2022-08-25 20:30:35 +0300

    refactor: delete Button component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,8 @@
 import type { ReactNode } from 'react';
 
-interface IButtonProps {
-  children: ReactNode;
-  openForm: () => void;
-}
-
 interface INavigationItemProps {
   children: ReactNode;
 }
-
-const Button = ({ children, openForm }: IButtonProps) => (
-  <button
-    type='button'
-    className='text-xl bg-orange-400/90 rounded-full px-3 py-1 transition-colors hover:bg-orange-500'
-    onClick={openForm}
-  >
-    {children}
-  </button>
-);
 
 const Logo = () => <h1 className='text-3xl'>Logo</h1>;
 
@@ -45,7 +30,13 @@ const Header = ({ handleOpen }: IHeaderProps) => (
     <div className='ml-auto'>
       <Navigation />
     </div>
-    <Button openForm={handleOpen}>Login</Button>
+    <button
+      type='button'
+      className='text-xl bg-orange-400/90 rounded-full px-3 py-1 transition-colors hover:bg-orange-500'
+      onClick={handleOpen}
+    >
+      Login
+    </button>
   </div>
 );
 

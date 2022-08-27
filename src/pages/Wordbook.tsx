356f693917b6c7b2a199937ed90c788e68d24955
thelastandrew <thelastandrew@gmail.com>
2022-08-27 10:53:26 +0300

    feat: set aproximate page structure

--- src/pages/Wordbook.tsx
+++ src/pages/Wordbook.tsx
@@ -1,8 +1,30 @@
 import PageTitle from 'components/PageTitle';
 
+const GroupSelector = () => <div>select group</div>;
+const Pagination = () => <div>select page</div>;
+const Options = () => <div>select options</div>;
+const Word = () => <div>this is a word</div>;
+const NavBlock = () => (
+  <div className='flex'>
+    <GroupSelector />
+    <Pagination />
+    <Options />
+  </div>
+);
+const WordsList = () => (
+  <div>
+    <Word />
+    <Word />
+    <Word />
+    <Word />
+  </div>
+);
+
 const Wordbook = () => (
   <div className='py-2 px-2 w-full'>
     <PageTitle>Wordbook</PageTitle>
+    <NavBlock />
+    <WordsList />
   </div>
 );
 

5a2725820cbce321db4be796bc54eea2e879d047
NikitaKakurin <neyasut87@rambler.ru>
2022-09-03 20:37:52 +0300

    feat: load words

--- src/pages/Sprint.tsx
+++ src/pages/Sprint.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import speaker from 'assets/svg/speaker.svg';
 import { useLocation } from '@tanstack/react-location';
 import API from 'API/API';
+import { IWord } from 'interfaces/apiData';
 
 interface IInitBackgroundProps {
   initCount: number;
@@ -64,6 +65,54 @@ const InitBackground = ({ initCount, hideInitBackground }: IInitBackgroundProps)
     </div>
   );
 };
+interface ITimerProps {
+  initCount: number;
+  finishGame: () => void;
+}
+
+const Timer = ({ initCount, finishGame }: ITimerProps) => {
+  const [initCountState, setInitCountState] = useState(initCount);
+  useEffect(() => {
+    const showTimer = () => {
+      const InitTimer = setTimeout(() => {
+        if (initCountState <= 0) {
+          finishGame();
+          return;
+        }
+        setInitCountState((prev) => prev - 1);
+      }, 1000);
+      return InitTimer;
+    };
+
+    const timer = showTimer();
+    return () => clearTimeout(timer);
+  }, [initCountState, finishGame]);
+
+  const calcPercent = () => {
+    const percent = Math.floor((initCountState * 100) / 60);
+    const remainder = 100 - percent;
+    return {
+      percent,
+      remainder,
+    };
+  };
+  const { percent, remainder } = calcPercent();
+  const timerStyle = `linear-gradient(to left top,#030397 ${percent - 1}%, #adf2f7 ${
+    remainder + 1
+  }%,#adf2f7 ${remainder}%)`;
+
+  return (
+    <span
+      style={{
+        borderImageSource: timerStyle,
+        borderImageSlice: '1',
+      }}
+      className='flex justify-center items-center w-[50px] h-[50px] mt-2 rotate-45 rounded-full border-4 text-slate-900 transition-all duration-500 text-2xl font-bold'
+    >
+      <span className='-rotate-45'>{initCountState}</span>
+    </span>
+  );
+};
 
 interface IComplexityBtnProps {
   value: number;
@@ -99,19 +148,64 @@ const Complexity = ({ setComplexity }: IComplexityProps) => (
   </div>
 );
 interface IGameProps {
-  resWords: () => IWord[];
+  words: IWord[];
 }
-const Game = () => {
-  const [first, setfirst] = useState('');
+const Game = ({ words }: IGameProps) => {
+  const [endGame, setEndGame] = useState(false);
+  const [indexWord, setIndexWord] = useState(0);
+  const sortWords = (wordsForSort: IWord[]) => {
+    const max = wordsForSort.length - 1;
+    return wordsForSort
+      .map((word, index) => {
+        const rightAnswer = !!Math.round(Math.random());
+        if (rightAnswer) {
+          return {
+            word,
+            wordTranslate: word.wordTranslate,
+            rightAnswer: true,
+          };
+        }
+        let random = Math.floor(Math.random() * max);
+        while (random === index) {
+          random = Math.floor(Math.random() * max);
+        }
+        return {
+          word,
+          wordTranslate: wordsForSort[random].wordTranslate,
+          rightAnswer: false,
+        };
+      })
+      .sort(() => 0.5 - Math.random());
+  };
+
+  const [sortedArr, setSortedArr] = useState(sortWords(words));
+  const finishGame = () => setEndGame(true);
+  const checkAnswer = (answer: boolean) => {
+    const { word, rightAnswer } = sortedArr[indexWord];
+    const result = answer === rightAnswer;
+
+    if (indexWord < sortedArr.length - 1) {
+      setIndexWord((prev) => prev + 1);
+    }
+  };
+  const handleKeypress = (e: KeyboardEvent) => {
+    if (e.key === 'ArrowRight') checkAnswer(true);
+    if (e.key === 'ArrowLeft') checkAnswer(false);
+  };
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeypress);
+    return () => {
+      document.removeEventListener('keydown', handleKeypress);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [indexWord]);
 
   return (
     <div className='w-full'>
       <div className='flex justify-between items-center w-full pb-10'>
-        <div className='flex justify-center items-center w-[75px] h-[75px] rounded-full border-4 border-slate-100 font-medium text-xl'>
-          59
-        </div>
+        <Timer initCount={59} finishGame={finishGame} />
         <div className='flex justify-center items-center w-[75px] h-[75px] rounded-full border-4 border-blue-600 font-medium text-xl'>
-          2800
+          0
         </div>
         <div className='bg-sprint-close w-10 h-10 bg-contain bg-no-repeat hover:invert cursor-pointer' />
       </div>
@@ -151,12 +245,17 @@ const Game = () => {
             </div>
           </div>
 
-          <span className='text-2xl font-semibold text-center'>crowded</span>
-          <span className='text-xl font-medium text-center'>переполненый</span>
+          <span className='text-2xl font-semibold text-center'>
+            {sortedArr[indexWord].word.word}
+          </span>
+          <span className='text-xl font-medium text-center'>
+            {sortedArr[indexWord].wordTranslate}
+          </span>
 
           <div className='flex justify-evenly items-center gap-10'>
             <div className='flex flex-col justify-center items-center'>
               <button
+                onClick={() => checkAnswer(false)}
                 type='button'
                 className='w-[100px] h-[40px] rounded-xl bg-rose-400 text-bold hover:bg-rose-500/90'
               >
@@ -166,6 +265,7 @@ const Game = () => {
             </div>
             <div className='flex flex-col justify-center items-center'>
               <button
+                onClick={() => checkAnswer(true)}
                 type='button'
                 className='w-[100px] h-[40px] rounded-xl bg-green-400 text-medium hover:bg-green-500/90'
               >
@@ -209,39 +309,23 @@ const Game = () => {
 interface IStartGameProps {
   complexity: number;
 }
-interface IWord {
-  id: string;
-  group: number;
-  page: number;
-  word: string;
-  image: string;
-  audio: string;
-  audioMeaning: string;
-  audioExample: string;
-  textMeaning: string;
-  textExample: string;
-  transcription: string;
-  wordTranslate: string;
-  textMeaningTranslate: string;
-  textExampleTranslate: string;
-}
+
 const StartGame = ({ complexity }: IStartGameProps) => {
   const [isInitBackground, setInitBackgroundShow] = useState(true);
   const [words, setWords] = useState(Array<IWord>);
   const hideInitBackground = () => setInitBackgroundShow(() => false);
-  const setData = (data: IWord[]) => setWords(data);
   useEffect(() => {
-    const getWords = (group: number, page: number) => {
-      // const res: IWord[] = API.getWords(`${complexity - 1}`, '1');
-      // res.then(()=>setData(res)) ;
-    };
-    getWords(complexity, 1);
+    API.getWords(`${complexity - 1}`, '1')
+      .then((data) => setWords(data))
+      .catch((e: string) => {
+        throw new Error(e);
+      });
   }, [complexity]);
 
   return (
     <>
       {isInitBackground && <InitBackground initCount={4} hideInitBackground={hideInitBackground} />}
-      {!isInitBackground && <Game />}
+      {!isInitBackground && <Game words={words} />}
     </>
   );
 };
